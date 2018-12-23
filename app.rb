df687e20e1d3b99bce3e4c40e085edb5650547f0
Jad Abdallah <justcallmethis@gmail.com>
2018-12-23 03:07:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end 
   
-  post '/display_puppy' do 
+  post '/' do 
     @new_puppy = Puppy.new(params[:name], para,s[:breed], params[:age])
     erb :display_puppy
   end 
